14637ca3b79440099e4b5f6073d8bf93feb3766f
michael.budnick <michael.budnick@outlook.com>
2015-11-30 13:46:04 -0500

    Rename repo

--- inherits.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Function.prototype.inherits = function (SuperClass) {
-  var Surrogate = function () {};
-  Surrogate.prototype = SuperClass.prototype;
-  this.prototype = new Surrogate();
-  this.prototype.constructor = this;
-}
